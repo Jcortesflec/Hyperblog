f1440e659a19f88a6e4c2e0c5942cb53ff6c66fc
Juan Cortes <jcortesflec@gmail.com>
2022-07-12 16:44:59 -0500

    se construye el primer proyecto

---
+++ conversor/Main.py
@@ -0,0 +1,9 @@
+from moneda import colombia ,argentina, brazil
+
+opcion=input("¿Que moneda manejas en tu pais?\n\n1. Colombia \n2. Argentina \n3. Brazil \n\ningresa el numero de tu pais \n")
+if opcion == "1":
+    colombia()
+elif opcion == "2":
+    argentina()
+elif opcion == "3":
+    brazil()
---
+++ conversor/moneda.py
@@ -0,0 +1,28 @@
+
+
+def colombia():
+        pesos = input("¿cuantos pesos colombianos tienes? ")
+        pesos = float(pesos)
+        valor_dolar = 4608.65
+        dolares = pesos / valor_dolar
+        dolares = round(dolares , 2)
+        dolares = str(dolares)
+        print("tienes $"+dolares+" dolares")
+
+def argentina():
+        pesos = input("¿cuantos pesos argentinos tienes? ")
+        pesos = float(pesos)
+        valor_dolar = 127.59
+        dolares = pesos / valor_dolar
+        dolares = round(dolares , 2)
+        dolares = str(dolares)
+        print("tienes $"+dolares+" dolares")
+
+def brazil():
+        real = input("¿cuantos reales brasileños tienes? ")
+        real = float(real)
+        valor_dolar = 5.44
+        dolares = real / valor_dolar
+        dolares = round(dolares , 2)
+        dolares = str(dolares)
+        print("tienes $"+dolares+" dolares")
